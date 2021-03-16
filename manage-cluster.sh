cb70a9ca9cc681c0ba64a97e950163988c8de38e
Alec Gunny <alec.gunny@gmail.com>
2021-03-16 10:20:48 -0700

    cleaning up arg parsing and adding labels

--- manage-cluster.sh
+++ manage-cluster.sh
@@ -1,12 +1,19 @@
 #! /bin/bash -e
 
-while getopts "c:m:z:p:h" opt; do
+cmd=$1
+if [[ $cmd == -* ]] || [[ -z $cmd ]]; then
+    echo "Must provide command create or delete!"
+    exit 1
+elif [[ $cmd != "create" ]] && [[ $cmd != "delete" ]]; then
+    echo "Provided unrecognized command $cmd"
+    exit 1
+fi
+shift
+
+while getopts "c:z:p:h" opt; do
     case ${opt} in
         c )
             cluster=${OPTARG}
-            ;;
-        m )
-            mode=${OPTARG}
             ;;
         z )
             zone=${OPTARG}
@@ -16,10 +23,11 @@ while getopts "c:m:z:p:h" opt; do
             ;;
         h )
             echo "Create or delete a GKE cluster"
+            echo "Usage: ./manage-cluster.sh CMD [options]"
+            echo "CMD: either create or delete"
             echo "Options:"
             echo "--------"
             echo "    -c: cluster name"
-            echo "    -m: mode, create or delete"
             echo "    -z: zone"
             echo "    -p: GCP project name"
             exit 0
@@ -32,29 +40,21 @@ while getopts "c:m:z:p:h" opt; do
 done
 shift $((OPTIND -1))
 
-if [[ -z "$zone" ]]; then
-    echo "No zone specified!"
-    exit 1
-fi
+: ${cluster:?Must specify cluster name}
+: ${zone:?Must specify zone}
+: ${project:?Must specify project}
 
-if [[ -z "$project" ]]; then
-    echo "No project specified!"
-fi
-
-if [[ -z "$mode" ]]; then
-    echo "No mode specified!"
-    exit 1
-elif [[ "$mode" == "create" ]]; then
+if [[ $cmd == "create" ]]; then
     gcloud container clusters create ${cluster} --num-nodes=2 --project=${project} --zone=${zone}
+
+    # add cluster credentials to k8s config
     gcloud container clusters get-credentials ${cluster} --project=${project} --zone=${zone}
-elif [[ "$mode" == "delete" ]]; then
+elif [[ $cmd == "delete" ]]; then
     gcloud container clusters delete ${cluster} --zone=${zone} --project=${project}
 
+    # also remove cluster credentials from kube config
     kubename=gke_${project}_${zone}_${cluster}
     kubectl config unset users.$kubename
     kubectl config unset contexts.$kubename
     kubectl config unset clusters.$kubename
-else
-    echo "Unrecognized mode ${mode}"
-    exit 1
 fi
