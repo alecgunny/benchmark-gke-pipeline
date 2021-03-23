d2adfa4921483f04e0c796df938ae4cb1185fceb
Alec Gunny <alec.gunny@gmail.com>
2021-03-23 12:22:19 -0700

    adding client scripts

---
+++ client.sh
@@ -0,0 +1,105 @@
+#! /bin/bash -e
+
+while getopts "c:p:z:b:G:g:v:N:r:i:h" opt; do
+    case ${opt} in
+        c )
+            cluster=${OPTARG}
+            ;;
+        p )
+            project=${OPTARG}
+            ;;
+        z )
+            zone=${OPTARG}
+            ;;
+        b )
+            bucket=${OPTARG}
+            ;;
+        G )
+            gpus_per_node=${OPTARG}
+            ;;
+        g )
+            gpus_per_deployment=${OPTARG}
+            ;;
+        v )
+            vcpus_per_gpu=${OPTARG}
+            ;;
+        N )
+            nodes=${OPTARG}
+            ;;
+        r )
+            rate=${OPTARG}
+            ;;
+        i )
+            iterations=${OPTARG}
+            ;;
+        h )
+            echo "Create a GPU node pool and deploy Triton server"
+            echo "deployments and load balancers onto it, then run"
+            echo "multi-client benchmarking scripts against this."
+            echo "Creates a separate deployment and load balancer"
+            echo "for each node so that clients can point to"
+            echo "ip addresses dedicated to their inference stream."
+            echo ""
+            echo "Options:"
+            echo "--------"
+            echo "    -c: cluster name for deploying Triton server"
+            echo "    -p: project name for deploying Triton server"
+            echo "    -z: zone for deploying Triton server"
+            echo "    -b: GCP bucket where Triton model repository is hosted"
+            echo "    -G: Desired number of GPUs per node in server node pool"
+            echo "    -g: Desired number of GPUs per deployment. Must be less than or equal to -G"
+            echo "    -N: Number of server nodes (and client instances) to leverage"
+            echo "    -r: Rate at which to generate requests from each client"
+            echo "    -i: Number of iterations over which to run client benchmarking"
+            exit 0
+            ;;
+        \? )
+            echo "Unrecognized argument ${opt}"
+            exit 1
+    esac
+done
+shift $((OPTIND -1))
+
+: ${cluster:?Must specify cluster name to deploy server on}
+: ${project:?Must specify GCP project for cluster}
+: ${zone:?Must specify zone for cluster}
+: ${bucket:?Must specify GCS bucket hosting model repo}
+: ${gpus_per_node:?Must specify number of GPUs to leverage per node in node pool}
+: ${gpus_per_deployment:?Must specify number of GPUs to leverage in server deployments}
+: ${vcpus_per_gpu:?Must specify number of vCPUs to leverage per GPU}
+
+nodes=${nodes:-1}
+for i in $(seq $nodes); do
+    if [[ $nodes == 1 ]]; then
+        name="tritonserver"
+    else
+        name="tritonserver-${i}"
+
+    ./start-server.sh \
+        -c ${cluster} \
+        -p ${project} \
+        -z ${zone} \
+        -b ${bucket} \
+        -G ${gpus_per_node} \
+        -g ${gpus_per_deployment} \
+        -v ${vcpus_per_gpu} \
+        -N ${nodes} \
+        -n ${name}
+done
+
+for i in $(seq $nodes); do
+    if [[ $nodes == 1 ]]; then
+        name="tritonserver"
+    else
+        name="tritonserver-${i}"
+
+    ip=$(kubectl get service $name -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    python client.py \
+        --url $ip:8001 \
+        --model-name gwe2e \
+        --model-version 1 \
+        --generation-rate ${rate} \
+        --num-iterations ${iterations} \
+        --warm-up 10 \
+        --file-prefix "node-${i}" &> node-${i}_output.log
+done
--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
-while getopts "c:p:z:b:g:v:N:h" opt; do
+while getopts "c:p:z:b:g:G:v:N:n:h" opt; do
     case ${opt} in
         c )
             cluster=${OPTARG}
@@ -15,13 +15,19 @@ while getopts "c:p:z:b:g:v:N:h" opt; do
             bucket=${OPTARG}
             ;;
         g )
-            gpus=${OPTARG}
+            gpus_per_deployment=${OPTARG}
+            ;;
+        G )
+            gpus_per_node=${OPTARG}
             ;;
         v )
-            vcpus=${OPTARG}
+            vcpus_per_gpu=${OPTARG}
             ;;
         N )
             nodes=${OPTARG}
+            ;;
+        n )
+            name=${OPTARG}
             ;;
         h )
             echo "Create or delete a GKE cluster"
@@ -31,9 +37,11 @@ while getopts "c:p:z:b:g:v:N:h" opt; do
             echo "    -p: project name for deploying Triton server"
             echo "    -z: zone for deploying Triton server"
             echo "    -b: GCP bucket for hosting repo"
-            echo "    -g: number of T4s to attach to nodes on nodepool"
+            echo "    -G: number of T4s to attach to nodes on nodepool"
+            echo "    -g: number of T4s to attach to nodes on deployment"
             echo "    -N: number of nodes to add to nodepool"
-            echo "    -v: number of vCPUs to attach to nodes on nodepool"
+            echo "    -n: name for the Triton deployment"
+            echo "    -v: number of vCPUs to attach to nodes and deployment per GPU"
             exit 0
             ;;
         \? )
@@ -43,15 +51,28 @@ while getopts "c:p:z:b:g:v:N:h" opt; do
 done
 shift $((OPTIND -1))
 
+[[ $gpus_per_node -ge $gpus_per_deployment ]] || echo "Too many gpus per deployment!"
+
+vcpus_per_node=$(($vcpus_per_gpu * $gpus_per_node))
 ./manage-node-pool.sh create \
         -n tritonserver-pool \
         -c ${cluster} \
         -p ${project} \
         -z ${zone} \
-        -g ${gpus} \
+        -g ${gpus_per_node} \
         -N ${nodes} \
-        -v ${vcpus} || echo "Triton node pool already created" 2>/dev/null
+        -v ${vcpus_per_node} || echo "Triton node pool already created" 2>/dev/null
+
+# if we asked for the full number of gpus in a deployment,
+# we won't have enough CPU, so take one off
+vcpus_per_deployment=$(($vcpus_per_gpu * $gpus_per_deployment))
+if [[ $vcpus_per_deployment == $vcpus_per_node ]]; then
+    vcpus_per_deployment=$(($vcpus_per_deployment - 1))
+fi
 
 python format_yaml.py apps/triton-server/deploy.yaml \
-    --gpus $gpus --vcpus $(($vcpus - 1)) --repo $bucket | kubectl apply -f -
+    --gpus ${gpus_per_deployment} \
+    --vcpus ${vcpus_per_deployment} \
+    --repo ${bucket} \
+    --name ${name} | kubectl apply -f -
 kubectl rollout status deployment/tritonserver
