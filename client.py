9840a3f30b625bd1b7693f439d8eee863acb7e6b
Alec Gunny <alec.gunny@gmail.com>
2021-04-13 09:37:15 -0700

    getting rid of unnecessary processes

--- client.py
+++ client.py
@@ -1,17 +1,18 @@
 import argparse
 import logging
 import os
+import time
 import typing
+from queue import Empty
 
 import numpy as np
-import pandas as pd
 import tritonclient.grpc as triton
 from stillwater import (
     DummyDataGenerator,
     MultiSourceGenerator,
     ThreadedMultiStreamInferenceClient
 )
-from stillwater.client.monitor import MonitoredMetricViolationException
+from stillwater.utils import ExceptionWrapper
 
 
 def _normalize_file_prefix(file_prefix):
@@ -68,27 +69,68 @@ def main(
         warm_up_client.infer(model_name, warm_up_inputs, str(model_version))
 
     file_prefix = _normalize_file_prefix(file_prefix)
-    monitor = client.monitor(
-        output_pipes,
-        server_monitor={
-            "output_file": f"{file_prefix}server-stats.csv",
-            "snapshotter_queue": queue_threshold_us,
-            "deepclean_l_queue": queue_threshold_us,
-            "deepclean_h_queue": queue_threshold_us,
-            "bbh_queue": queue_threshold_us
-        },
-        client_monitor={
-            "output_file": f"{file_prefix}client-stats.csv",
-            "latency": latency_threshold
-        },
-        timeout=10
-    )
-
     logging.info(
         f"Gathering performance metrics over {num_iterations} iterations"
     )
-    for (seq_id, x), i in zip(monitor, range(num_iterations)):
-        continue
+
+    num_packages_received = 0
+    bars = "|" + " " * 25 + "|"
+    max_msg = f" {num_iterations}/{num_iterations}"
+    max_len = len(bars) + len(max_msg)
+
+    client.start()
+    try:
+        while True:
+            for seq_id, pipe in output_pipes.items():
+                if not pipe.poll():
+                    continue
+                x = pipe.recv()
+                if isinstance(x, ExceptionWrapper):
+                    x.reraise()
+                num_packages_received += 1
+                if num_packages_received == num_iterations:
+                    break
+
+            num_equal_signs = num_packages_received * 25 // num_iterations
+            num_spaces = 25 - num_equal_signs
+            msg = "|" + "=" * num_equal_signs + " " * num_spaces + "|"
+            msg += f" {num_packages_received}/{num_iterations}"
+            num_spaces = " " * (max_len - len(msg))
+            print(msg + num_spaces, end="\r", flush=True)
+    finally:
+        client.stop()
+        client.join(1)
+        try:
+            client.close()
+        except ValueError:
+            client.terminate()
+            time.sleep(0.1)
+            client.close()
+            logging.warning("Client closed ungracefully")
+
+    with open(f"{file_prefix}client-stats.csv", "w") as f:
+        columns = [
+            "sequence_id",
+            "message_start",
+            "request_send",
+            "request_get",
+            "request_return",
+        ]
+        f.write(",".join(columns))
+        while True:
+            try:
+                measurements = client._metric_q.get_nowait()
+            except Empty:
+                break
+            if measurements[0] == "start_time":
+                start_time = measurements[1]
+                continue
+
+            measurements = list(measurements)
+            sequence_id = measurements.pop(0)
+            measurements = [i - start_time for i in measurements]
+            measurements = [sequence_id] + measurements
+            f.write(",".join(map(str, measurements)))
 
 
 if __name__ == "__main__":
@@ -211,26 +253,26 @@ if __name__ == "__main__":
         try:
             main(**flags)
             break
-        except MonitoredMetricViolationException as e:
-            logging.exception("Metric violation")
-            if "latency" not in str(e):
-                raise
-            else:
-                file_prefix = _normalize_file_prefix(flags["file_prefix"])
-                df = pd.read_csv(f"{file_prefix}server-stats.csv")
+        # except MonitoredMetricViolationException as e:
+        #     logging.exception("Metric violation")
+        #     if "latency" not in str(e):
+        #         raise
+        #     else:
+        #         file_prefix = _normalize_file_prefix(flags["file_prefix"])
+        #         df = pd.read_csv(f"{file_prefix}server-stats.csv")
 
-                df["step"] = df.index // 6
-                elapsed = df.groupby("step")[["interval"]].agg("mean").cumsum()
-                elapsed.columns = ["elapsed"]
-                df = df.join(elapsed, on="step")
-                df = df[df.elapsed < df.elapsed.max() - 2]
+        #         df["step"] = df.index // 6
+        #         elapsed = df.groupby("step")[["interval"]].agg("mean").cumsum()
+        #         elapsed.columns = ["elapsed"]
+        #         df = df.join(elapsed, on="step")
+        #         df = df[df.elapsed < df.elapsed.max() - 2]
 
-                if np.percentile(df.queue, 99) < 200:
-                    num_violations += 1
-                    logging.warning("Queue times stable, retrying")
-                    continue
-                else:
-                    raise
+        #         if np.percentile(df.queue, 99) < 200:
+        #             num_violations += 1
+        #             logging.warning("Queue times stable, retrying")
+        #             continue
+        #         else:
+        #             raise
         except Exception:
             logging.exception("Fatal error")
             raise
